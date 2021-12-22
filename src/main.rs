97affa22a4d89599e7a23faa79a22b59d31f038d
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2021-12-21 22:21:30 -0800

    solve p21

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod p1;
+mod p2;
 
 fn main() {
     // let out = p1::p11::solve();
     // assert_eq!(out, 1709);
 
-    let out = p1::p12::solve();
-    assert_eq!(out, 1761);
+    // let out = p1::p12::solve();
+    // assert_eq!(out, 1761);
+
+    let out = p2::p21::solve();
+    assert_eq!(out, 1484118);
 }
---
+++ src/p2.rs
@@ -0,0 +1,60 @@
+#![allow(dead_code)]
+
+const BASE_PATH: &str = "D:/rust-projects/advent_of_rust/src/data";
+
+enum Dir {
+  Unspecified,
+  Up,
+  Down,
+  Forward,
+}
+
+pub struct Command {
+  direction: Dir,
+  dist: i32,
+}
+
+pub fn parse_p2() -> Vec<Command> {
+  let filename = crate::p2::BASE_PATH.to_owned() + "/p2.txt";
+  // println!("filename = {}", filename);
+
+  let contents = std::fs::read_to_string(filename).expect("Somenthing went wrong reading file");
+  contents
+    .split("\n")
+    .map(|command_str| {
+      let dir_dist: Vec<&str> = command_str.trim().split(" ").collect();
+      let dir = match dir_dist[0] {
+        "forward" => Dir::Forward,
+        "up" => Dir::Up,
+        "down" => Dir::Down,
+        _ => Dir::Unspecified,
+      };
+      let dist = dir_dist[1].parse().expect("not a number");
+      Command {
+        direction: dir,
+        dist: dist,
+      }
+    })
+    .collect()
+}
+
+pub mod p21 {
+  // product of horizontal and vert
+  pub fn solve() -> i32 {
+    let commands = crate::p2::parse_p2();
+    let mut horiz = 0;
+    let mut vert = 0;
+
+    for command in commands {
+      match command.direction {
+        crate::p2::Dir::Up => vert -= command.dist,
+        crate::p2::Dir::Down => vert += command.dist,
+        crate::p2::Dir::Forward => horiz += command.dist,
+        _ => (),
+      }
+    }
+
+    println!("horiz = {}, vert = {}", horiz, vert);
+    horiz * vert
+  }
+}
